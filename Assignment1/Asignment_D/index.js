3a652fdb71162e57982b470148e2edbf46d5e47e
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-04-21 14:41:20 +0100

    assignment D finished

--- Assignment1/Asignment_D/index.js
+++ Assignment1/Asignment_D/index.js
@@ -2,12 +2,57 @@ var array = ["apple", "banana", "pear", "chicken", "fish", "corn", "chips", "pot
 
 var arrayLength = array.length;
 
+for(var j = 0; j < arrayLength; j++){
+    document.write("<p>" + array[j] + "</p>");
+}
+
+document.write("After change: <br>");
+
+array[8] = "sugar";
+
 for(var i = 0; i < arrayLength; i++){
-    document.write(array[i] + "<br>");
+    document.write("<p>" + array[i] + "</p>");
 }
 
-if(array[8] == "salt"){
-    array[8].replace("sugar");
-}
 
- //need to see how to change the value
+
+// This is the bonus where we can change an element in the shopping list with a table. 
+//All of this was found and done following a tutorial online.
+
+var table = document.getElementById('shopping'); //id of table
+var cells = table.getElementsByTagName('td'); // HTMl tag of the element we want to use 
+
+for(var i = 0; i < cells.length; i++){ //iterate through all of the cells in the table
+    cells[i].onclick = function() {
+
+        if(this.hasAttribute('data-clicked')){ 
+            //if it has been clicked return value
+            return;
+        }
+
+        this.setAttribute('data-clicked', 'yes'); //setting attributes for editing.
+        this.setAttribute('data-text', this.innerHTML);
+
+        var input = document.createElement('input'); //get a way to get input
+        input.setAttribute('type', 'text'); //type of input
+        input.value = this.innerHTML; //this refers to the cell we are clicking
+
+        input.onblur = function(){
+            var td = input.parentElement; //getting the HTML parent element
+            var originalText = input.parentElement.getAttribute('data-text');
+            var currentText = this.value; //this is the input element
+
+            if(originalText != currentText){
+                //there is a change in the text
+                //Here the data can be saved into the data. 
+                td.removeAttribute('data-clicked');
+                td.removeAttribute('data-text');
+                td.innerHTML = currentText;
+            }
+
+        }
+        this.innerHTML = '';
+        this.append(input);
+        this.firstElementChild.select();
+    }
+}
