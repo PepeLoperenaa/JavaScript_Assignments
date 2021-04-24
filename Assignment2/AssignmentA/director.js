af8bcf70fd78d2ff89facd91a4990947ab985aee
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-04-24 12:11:55 +0100

    added getters in the last 2 classes

--- Assignment2/AssignmentA/director.js
+++ Assignment2/AssignmentA/director.js
@@ -1,5 +1,17 @@
 var director = {
     name: 'bla bla',
     age: 25,
-    work: ['avengers', 'The last Kingdom', 'The boys']
+    work: ['avengers', 'The last Kingdom', 'The boys'],
+
+    getName: function(){
+        return this.name;
+    },
+
+    getAge: function(){
+        return this.age;
+    },
+
+    getWorkDone: function(){
+        return this.work;
+    }
 }
--- Assignment2/AssignmentA/episodes.js
+++ Assignment2/AssignmentA/episodes.js
@@ -1,4 +1,12 @@
 var episodes = {
     name: 'The death of King Alfred',
     duration: 50.25,
+
+    getName: function(){
+        return this.name;
+    },
+
+    getDuration: function(){
+        return this.duration;
+    }
 };
