9faeeeb7a07ce39e44d228dcbbc5d8dd256135fa
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-04-24 11:54:31 +0100

    added methods to series

--- Assignment2/AssignmentA/series.js
+++ Assignment2/AssignmentA/series.js
@@ -4,3 +4,19 @@ var series  = {
     continue : true, 
     cast : cast
 };
+
+getName: function(){
+    return this.name;
+}
+
+getEpisodes: function(){
+    return this.episodes;
+}
+
+getContinue: function(){
+    return this.continue;
+}
+
+getCast: function(){
+    return this.cast;
+}
