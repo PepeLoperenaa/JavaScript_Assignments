9862c03bc0cb28f3797ece03343a28d04b713456
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-10 11:47:39 +0100

    added keyboard

--- Assignment4/AssignmentB/main.js
+++ Assignment4/AssignmentB/main.js
@@ -7,8 +7,22 @@ let guessed = [];
 
 function randWord(){
     answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
 
-    alert(answer);
+function generateButtons(){
+    let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => 
+        `
+            <button 
+            class = "btn btn-lg btn-primary m-2"
+            id = '`+ letter + `'
+            onClick = "handleGuess('`+ letter + `')"
+            >
+                ` + letter + `
+            </button>
+        `).join('');
+
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
 randWord();
+generateButtons();
