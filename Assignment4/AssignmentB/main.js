ace86259db1e873e142486ee605ad748666622be
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-10 17:36:41 +0100

    assignment 4 b made

--- Assignment4/AssignmentB/main.js
+++ Assignment4/AssignmentB/main.js
@@ -2,15 +2,16 @@ var programming_languages = ["java", "csharp", "html", "javascript", "php", "pyt
 
 let answer = '';
 let maxWrong = 6;
-let mistakes = 0; 
+let mistakes = 0;
 let guessed = [];
+let wordStatus = null;
 
-function randWord(){
+function randWord() {
     answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
 
-function generateButtons(){
-    let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => 
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuwxyz'.split('').map(letter =>
         `
             <button 
             class = "btn btn-lg btn-primary m-2"
@@ -19,10 +20,67 @@ function generateButtons(){
             >
                 ` + letter + `
             </button>
-        `).join('');
+        `).join(''); //gets rid of all of the commas. 
 
     document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes
+}
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null; //lambda
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(chosenLetter) >= 0) { //if it is true
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
+    }
+}
+
+function updateHangmanPicture() {
+    document.getElementsById('hangman_pic').src = './images/' + mistakes + '.jpg';
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = "The answer was: " + answer;
+        document.getElementById('keyboard').innerHTML = 'You lost >:|';
+    }
+}
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'You have won this game of hangman!!';
+    }
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangman_pic').src = './images/0.jpg';
+
+    randWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
 randWord();
 generateButtons();
+guessedWord();
+handleGuess();
