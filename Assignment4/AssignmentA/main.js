0591cac2325ea42afde1407d5fc0d4ec809fd85c
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-11 18:26:41 +0100

    comments again assignment 4

--- Assignment4/AssignmentA/main.js
+++ Assignment4/AssignmentA/main.js
@@ -1,16 +1,16 @@
 function numbers() {
-    let div = document.getElementById('new');
-    let a0 = 0;
+    let div = document.getElementById('new'); //get html element
+    let a0 = 0; //first number fibonacci 
     let a1 = 1;
-    var element = document.createElement("p");
-    element.innerHTML = a0;
-    div.appendChild(element);
+    var element = document.createElement("p"); //creating html tag 
+    element.innerHTML = a0; //HTMl value
+    div.appendChild(element); //put together so that it can be shown in html. 
     element = document.createElement("p");
     element.innerHTML = a1;
     div.appendChild(element);
 
-    for(let i=0;i<18;i++) {
-        let current = a0+a1;
+    for (let i = 0; i < 18; i++) { //running through the first 20 fibonacci numbers. 
+        let current = a0 + a1;
         element = document.createElement("p");
         element.innerHTML = current;
         div.appendChild(element);
