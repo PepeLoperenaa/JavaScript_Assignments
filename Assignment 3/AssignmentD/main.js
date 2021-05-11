7b6337ac5d339686e83d283aeccdde56e698f152
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-11 18:17:14 +0100

    check assignment 3 and it is workin

--- Assignment 3/AssignmentD/main.js
+++ Assignment 3/AssignmentD/main.js
@@ -1,39 +1,39 @@
 var items = [];
 
 function Item(text) {
-    this.text=text;
-    this.done=false;
+    this.text = text;
+    this.done = false;
 
-    this.getText = function() {
+    this.getText = function () {
         return this.text;
-    }
+    };
 
-    this.setDone = function() {
-        if(this.done==false) {
+    this.setDone = function () {
+        if (this.done == false) {
             this.done = true;
             return true;
         }
         else
             return false;
     }
-}
+};
 
-$(function() {
-    $('#button').on('click', function() {
+$(function () {
+    $('#button').on('click', function () {
         $('#todo').append("<li><input type='text' id='current'></input></li>"); //input
         $('#current').focus();
-        $('#current').on('focusout', function() {
+        $('#current').on('focusout', function () {
             items.push(new Item(this.value));
             $('#todo > li').last().remove();
-            $('#todo').append("<li class='item'>"+items[items.length-1].getText()+"</li>");
+            $('#todo').append("<li class='item'>" + items[items.length - 1].getText() + "</li>");
         })
     });
 
-    $('#todo').on('click','li', function(e) { //need to remove after strike. 
+    $('#todo').on('click', 'li', function (e) { //need to remove after strike.
         $(this).contents().wrap("<strike>");
         let idx = $(this).index(); //get index of this li
         let done = items[idx].setDone();
-        if(!done) {
+        if (!done) {
             $(this).remove();   // delete element from DOM
             items.splice(idx, 1); //delete element from array
         }
